18088d28df9113bc9a89baa3a6245d20776dd1d0
hasansinan17 <45516557+hasansinan17@users.noreply.github.com>
2018-12-23 14:43:29 -0500

    Add files via upload

---
+++ python-challange/PyBank/main.py.py
@@ -0,0 +1,56 @@
+import csv
+
+loadfile = "Resources/budget_data.csv"
+outputfile = "output/Financial_Analysis.txt"
+
+total_months = 0
+total_revenue = 0
+prev_revenue = 0
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+
+month_of_change = []
+revenue_change_list = []
+
+with open(loadfile,newline="") as budget_data:
+    reader = csv.DictReader(budget_data)
+
+    for row in reader:
+
+        total_months = total_months + 1
+        total_revenue = total_revenue + int(row["Profit/Losses"])
+
+        revenue_change = int(row["Profit/Losses"]) - prev_revenue
+        prev_revenue = int(row["Profit/Losses"])
+        revenue_change_list = revenue_change_list + [revenue_change]
+        month_of_change = month_of_change + [row["Date"]]
+
+
+        if (revenue_change > greatest_increase[1]):
+            greatest_increase[0] = row["Date"]
+            greatest_increase[1] = revenue_change
+
+
+        if (revenue_change < greatest_decrease[1]):
+            greatest_decrease[0] = row["Date"]
+            greatest_decrease[1] = revenue_change
+
+
+revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
+
+
+output = (
+    f"\nFinancial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total Revenue: ${total_revenue}\n"
+    f"Average Revenue Change: ${revenue_avg}\n"
+    f"Greatest Increase in Revenue: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Revenue: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+
+print(output)
+
+
+with open(outputfile, "w") as txt_file:
+    txt_file.write(output)
